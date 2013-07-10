a60572229b2e2833315d542b81c1b57ce6522315
MakerSquare Student <students@themakersquare.com>
2013-07-10 12:33:42 -0500

    adding models and migrations for comments

---
+++ db/migrate/20130710172732_add_comments_to_links_and_users.rb
@@ -0,0 +1,10 @@
+class AddCommentsToLinksAndUsers < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.references :user
+      t.references :link
+      t.text :message
+      t.timestamps
+    end
+  end
+end
